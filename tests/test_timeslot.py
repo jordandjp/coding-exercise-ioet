b2013d54d5fc925bc7c4a54d736f51b1d6048566
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-07 13:21:07 -0500

    test: add test for timeslot total_hours method

--- tests/test_timeslot.py
+++ tests/test_timeslot.py
@@ -9,6 +9,7 @@ class TimeslotTestCase(unittest.TestCase):
         self.morning = Timeslot(DayTime(6, 0), DayTime(12, 0))
         self.afternoon = Timeslot(DayTime(12, 1), DayTime(18, 0))
         self.employee_timeslot = Timeslot(DayTime(14, 0), DayTime(16, 0))
+        self.employee2_timeslot = Timeslot(DayTime(13, 30), DayTime(15, 15))
 
     def test_employee_timeslot_intersect_with(self):
         self.assertTrue(self.employee_timeslot.intersect_with(self.afternoon))
@@ -26,15 +27,20 @@ class TimeslotTestCase(unittest.TestCase):
         self.assertEqual(self.afternoon.intersection(self.morning), -1)
 
     def test_employees_intersection(self):
-        employee2_timeslot = Timeslot(DayTime(13, 30), DayTime(15, 15))
         self.assertEqual(
-            employee2_timeslot.intersection(self.employee_timeslot),
+            self.employee2_timeslot.intersection(self.employee_timeslot),
             Timeslot(DayTime(14, 0), DayTime(15, 15)),
         )
 
     def test_timeslot_strptime(self):
         timeslot = Timeslot.strptime("18:00", "22:00", "%H:%M")
         self.assertEqual(timeslot, Timeslot(DayTime(18, 0), DayTime(22, 0)))
+
+    def test_total_hours(self):
+        self.assertEqual(self.morning.total_hours, 6)
+        self.assertEqual(self.afternoon.total_hours, 6 - (1 / 60))
+        self.assertEqual(self.employee_timeslot.total_hours, 2)
+        self.assertEqual(self.employee2_timeslot.total_hours, 1.75)
 
 
 if __name__ == "__main__":
