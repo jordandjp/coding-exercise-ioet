4cfdafd5040e6e2beb77fea876dab66a64b71c6f
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-07 18:45:26 -0500

    feat: Add fundamental abstractions (value objects)

--- src/model.py
+++ src/model.py
@@ -1,9 +1,24 @@
 from __future__ import annotations
 
+import calendar
 from dataclasses import dataclass
+from enum import Enum
+from typing import List
+
+from src.timeslot import Timeslot
 
 
-@dataclass
+class Day(Enum):
+    MONDAY = calendar.MONDAY
+    THURSDAY = calendar.THURSDAY
+    WEDNESDAY = calendar.WEDNESDAY
+    TUESDAY = calendar.TUESDAY
+    FRIDAY = calendar.FRIDAY
+    SATURDAY = calendar.SATURDAY
+    SUNDAY = calendar.SUNDAY
+
+
+@dataclass(frozen=True)
 class Money:
     value: int
     currency: str = "USD"
@@ -19,3 +34,46 @@ class Money:
 
     def __rmul__(self, other: int) -> Money:
         return self * other
+
+
+@dataclass(frozen=True)
+class DayTimeslot:
+    day: Day
+    timeslot: Timeslot
+
+
+@dataclass(frozen=True)
+class TimeslotPayRate:
+    day_timeslot: DayTimeslot
+    hourly_rate: Money
+
+
+@dataclass(frozen=True)
+class TimeslotPayRate:
+    day_timeslot: DayTimeslot
+    pay_rate: Money
+
+
+@dataclass(frozen=True)
+class Employee:
+    name: str
+
+
+@dataclass(frozen=True)
+class EmployeeTimeslots:
+    employee: Employee
+    days_timeslot: List[DayTimeslot]
+
+
+@dataclass(frozen=True)
+class Pay:
+    amount: Money
+    employee: Employee
+
+
+class PayTable:
+    """An abstraction that represents the information of the payment rate
+    per time interval of the days of the week."""
+
+    def calculate_employee_pay(self, employee_timeslots: EmployeeTimeslots) -> Pay:
+        """Calculate the total amount to be paid for the employee's hours worked"""
