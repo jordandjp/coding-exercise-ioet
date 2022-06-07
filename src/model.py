6aa947b0e460fcb9b537911762ccc97b9a9f7042
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-07 12:52:01 -0500

    feat: Implemented Money data access methods

--- src/model.py
+++ src/model.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
 
@@ -5,3 +7,15 @@ from dataclasses import dataclass
 class Money:
     value: int
     currency: str = "USD"
+
+    def __add__(self, other: Money) -> Money:
+        if self.currency != other.currency:
+            ValueError("Money objects must be of the same currency")
+
+        return Money(self.value + other.value, self.currency)
+
+    def __mul__(self, other: int) -> Money:
+        return Money(self.value * other, self.currency)
+
+    def __rmul__(self, other: int) -> Money:
+        return self * other
