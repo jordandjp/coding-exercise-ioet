baa69ac3bda5623dcef98529ec622adbad2eb148
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-08 18:11:55 -0500

    style: Improve returned type of read_txt_employee_data

--- src/readers.py
+++ src/readers.py
@@ -1,8 +1,8 @@
 from os import PathLike
-from typing import Union
+from typing import List, Union
 
 
-def read_txt_employee_data(file: Union[str, bytes, PathLike]) -> list:
+def read_txt_employee_data(file: Union[str, bytes, PathLike]) -> List[str]:
     """Read a .txt file into a list of raw strings representing timeslots entries"""
     with open(file, mode="rt", encoding="utf-8") as f:
         data = f.read().split("\n")
