564bea62006a39efd39d831fc32003b6a1086375
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-07 20:46:17 -0500

    refactor: refactor calculate_employee_pay and add calculate_singlentimeslot_pay method

--- src/model.py
+++ src/model.py
@@ -44,7 +44,7 @@ class DayTimeslot:
     def intersect_with(self, other: DayTimeslot) -> bool:
         return self.day == other.day and self.timeslot.intersect_with(other.timeslot)
 
-    def intersection(self, other: DayTimeslot) -> bool:
+    def intersection(self, other: DayTimeslot) -> DayTimeslot | int:
         result = -1
         if self.intersect_with(other):
             result = DayTimeslot(self.day, self.timeslot ^ other.timeslot)
@@ -85,22 +85,31 @@ class PayTable:
     per time interval of the days of the week."""
 
     def __init__(
-        self, timeslots_payrate: List[TimeslotPayRate], currency: str = "USD"
+        self, timeslots_pay_rate: List[TimeslotPayRate], currency: str = "USD"
     ) -> None:
-        self.timeslots_payrate = timeslots_payrate
+        self.timeslots_pay_rate = timeslots_pay_rate
         self.currency = currency
 
     def calculate_employee_pay(self, employee_timeslots: EmployeeTimeslots) -> Pay:
         """Calculate the total amount to be paid for the employee's hours worked"""
         money = Money(0, self.currency)
-        for employee_dts in employee_timeslots.days_timeslot:
-            for timeslot_payrate in self.timeslots_payrate:
-                if (
-                    employee_dts.day == timeslot_payrate.day_timeslot.day
-                    and employee_dts.timeslot.intersect_with(
-                        timeslot_payrate.day_timeslot.timeslot
-                    )
-                ):
-                    ts = employee_dts.timeslot ^ timeslot_payrate.day_timeslot.timeslot
-                    money = money + (ts.total_hours * timeslot_payrate.pay_rate)
+
+        for employee_day_timeslot in employee_timeslots.days_timeslot:
+            single_pay = self.calculate_single_timeslot_pay(
+                employee_day_timeslot, employee_timeslots.employee
+            )
+            money = money + single_pay.amount
+
         return Pay(money, employee_timeslots.employee)
+
+    def calculate_single_timeslot_pay(
+        self, day_timeslot: DayTimeslot, employee: Employee
+    ) -> Pay:
+        money = Money(0, self.currency)
+
+        for timeslot_pay_rate in self.timeslots_pay_rate:
+            if day_timeslot.intersect_with(timeslot_pay_rate.day_timeslot):
+                dts = day_timeslot.intersection(timeslot_pay_rate.day_timeslot)
+                money = money + (dts.timeslot.total_hours * timeslot_pay_rate.pay_rate)
+
+        return Pay(money, employee)
