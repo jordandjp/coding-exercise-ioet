c033e2c5ea60190ea3f13fffe00c39d851172f54
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-08 10:56:37 -0500

    feat: add acme_table data and load_csv_table function

---
+++ src/load_table.py
@@ -0,0 +1,34 @@
+import csv
+from os import PathLike
+from typing import Union
+
+from src.model import Day, DayTimeslot, Money, PayTable, TimeslotPayRate
+from src.timeslot import Timeslot
+
+
+def load_csv_table(file: Union[str, bytes, PathLike]) -> PayTable:
+
+    day_map = {
+        "MO": Day.MONDAY,
+        "TU": Day.TUESDAY,
+        "WE": Day.WEDNESDAY,
+        "TH": Day.THURSDAY,
+        "FR": Day.FRIDAY,
+        "SA": Day.SATURDAY,
+        "SU": Day.SUNDAY,
+    }
+
+    with open(file, mode="rt", encoding="utf-8", newline="") as csvfile:
+        reader = csv.DictReader(csvfile)
+        pay_rate_timeslots = []
+
+        for row in reader:
+            timeslot = Timeslot.strptime(row["start_time"], row["end_time"])
+            day_timeslot = DayTimeslot(day_map[row["day"]], timeslot)
+            pay_rate_timeslots.append(
+                TimeslotPayRate(
+                    day_timeslot, Money(int(row["hourly_rate"]), row["currency"])
+                )
+            )
+
+        return PayTable(pay_rate_timeslots)
