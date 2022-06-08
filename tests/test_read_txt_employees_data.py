99340da6d8d8d1791014067cbcd0881ec065d597
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-08 13:10:31 -0500

    test: add read_txt_employee_data tests

---
+++ tests/test_read_txt_employees_data.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+from src.readers import read_txt_employee_data
+
+
+class ReadTxtEmployeeDataTestCase(unittest.TestCase):
+    def setUp(self):
+        self.raw_data = read_txt_employee_data("tests/data/test_input_data.txt")
+
+    def test_len_elements_equal_num_lines(self):
+        self.assertEqual(len(self.raw_data), 5)
+
+    def test_first_line(self):
+        self.assertEqual(
+            self.raw_data[0],
+            "TOM=TH15:00-22:00,TU01:00-20:15,SU16:30-23:45,WE09:30-21:30",
+        )
+
+    def test_not_new_line(self):
+        for line in self.raw_data:
+            with self.subTest(line=line):
+                self.assertFalse("\n" in line)
+
+
+if __name__ == "__main__":
+    unittest.main()
