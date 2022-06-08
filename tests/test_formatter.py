d695cd6b99fcbe1301ad24b2465c82fa00a7684f
jordandjp <64615330+jordandjp@users.noreply.github.com>
2022-06-07 21:43:17 -0500

    test: add formatter tests

---
+++ tests/test_formatter.py
@@ -0,0 +1,37 @@
+import unittest
+from src.model import Employee, Money, Pay
+
+from src.formatters import StringFormatter
+
+
+class FormatterTestCase(unittest.TestCase):
+    def setUp(self):
+        self.one_line_formatter = StringFormatter(
+            "The amount to pay {obj.employee.name} is: {obj.amount.value} {obj.amount.currency}",
+            "obj",
+        )
+        self.multi_line_formatter = StringFormatter(
+            "Payment:\n"
+            "\tName: {pay.employee.name}\n"
+            "\tAmount: {pay.amount.value}\n"
+            "\tCurrency: {pay.amount.currency}\n",
+            "pay",
+        )
+        self.rene_pay = Pay(Money(35, "USD"), Employee("RENE"))
+        self.bob_pay = Pay(Money(30, "GBP"), Employee("Bob"))
+
+    def test_one_line_format(self):
+        self.assertMultiLineEqual(
+            self.one_line_formatter.format(self.rene_pay),
+            "The amount to pay RENE is: 35 USD",
+        )
+
+    def test_multi_line_formatter(self):
+        self.assertMultiLineEqual(
+            self.multi_line_formatter.format(self.bob_pay),
+            "Payment:\n\tName: Bob\n\tAmount: 30\n\tCurrency: GBP\n",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
